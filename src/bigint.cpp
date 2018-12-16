d38af45e3f0dbfd26c9c06617c351c4905450594
fahaas <fahaas@edu.aau.at>
2018-12-16 20:44:08 +0100

    changed definition of bigint; added implementation of bigint constructors

--- src/bigint.cpp
+++ src/bigint.cpp
@@ -1,2 +1,101 @@
 //(C) 2018 fahaas
+/*!
+ *	\file bigint.cpp
+ *	\brief implementation of bigint class
+ */
 #include "bigint.hpp"
+#include <algorithm>
+#include <array>
+#include <limits>
+
+constexpr std::array<uint64_t, 16> p16 = {1,
+                                          16,
+                                          256,
+                                          4096,
+                                          65536,
+                                          1048576,
+                                          16777216,
+                                          268435456,
+                                          4294967296,
+                                          68719476736,
+                                          1099511627776,
+                                          17592186044416,
+                                          281474976710656,
+                                          4503599627370496,
+                                          72057594037927936,
+                                          1152921504606846976};
+
+namespace numeric {
+    template <typename data_type, class vector_allocator>
+    bigint<data_type, vector_allocator>::bigint(data_type n) noexcept {
+        data.push_back(n);
+    }
+
+    template <typename data_type, typename vector_allocator>
+    bigint<data_type, vector_allocator>::bigint(std::int64_t n) noexcept {
+        auto is_min = false;
+
+        if (n == 0) {
+            data.push_back(0);
+            is_signed = false;
+            return;
+        }
+
+        if (n < 0) {
+            // an input value of -2^63 would lead to unexpected behaviour because abs(std::int64_t::max-1) ==
+            // abs(std::int64_t::min)
+            if (n == std::numeric_limits<std::int64_t>::min()) {
+                ++n;
+                is_min = true;
+            }
+            n *= -1;
+            is_signed = true;
+        }
+
+        // simple algorithm to convert to base
+        while (n != 0) {
+            data.push_back(static_cast<data_type>(n % base));
+            n -= n % base;
+            n /= base;
+        }
+
+        if (is_min)
+            this->operator--();
+    }
+
+    template <typename data_type, typename vector_allocator>
+    bigint<data_type, vector_allocator>::bigint(std::string hex_str)
+    {
+        std::reverse(hex_str.begin(), hex_str.end());
+        if (hex_str.back() == '-') {
+            is_signed = true;
+            hex_str.pop_back();
+        }
+
+        std::for_each(hex_str.begin(), hex_str.end(), [](auto& c) {
+            if (c >= 48 && c <= 57)
+                c -= 48;
+            else if (c >= 65 && c <= 90)
+                c -= 55;
+            else if (c >= 97 && c <= 122)
+                c -= 87;
+            else
+                throw std::runtime_error("Input string not valid!");
+        });
+
+        constexpr std::uint8_t block_size = sizeof(data_type) * 2;
+        data.resize(hex_str.size() / block_size + (hex_str.size() % block_size == 0 ? 0 : 1), 0);
+
+        std::uint8_t block_i = 0;
+        std::size_t this_data_i = 0;
+        for (char i : hex_str) {
+            data[this_data_i] += i * p16[block_i];
+            ++block_i;
+
+            if (block_i == block_size) {
+                ++this_data_i;
+                block_i = 0;
+            }
+        }
+    }
+} // namespace numeric
--- src/bigint.hpp
+++ src/bigint.hpp
@@ -1,94 +1,94 @@
 //(C) 2018 fahaas
 /*!
-*	\file bigint.hpp
-*	\brief implementation of bigint class
-*/
+ *	\file bigint.hpp
+ *	\brief A definition of the bigint class.
+ *  \details NOTE: Requires at least a 64-bit unsigned integer resulting in no support for 32-bit builds.
+ */
 #pragma once
 
+#include <boost/integer.hpp>
+#include <boost/operators.hpp>
 #include <limits>
 #include <string>
+#include <type_traits>
 #include <vector>
-#include <utility>
-#include <boost/integer.hpp>
 
 namespace numeric {
 
 #ifdef GNUC
-	using uint128_t = unsigned __int128; /*! < unsigned 128-bit integer */
-#endif // GNUC
+    using uint128_t = unsigned __int128; //!< unsigned 128-bit integer
+#endif
 
-	template <typename data_type, class vector_allocator = std::allocator<data_type>,
-		typename = typename std::enable_if<
-			std::is_integral<data_type>::value && std::is_unsigned<data_type>::value
+    template <typename data_type, class vector_allocator = std::allocator<data_type>>
+    class bigint : boost::arithmetic<bigint<data_type, vector_allocator>>,
+                   boost::unit_steppable<bigint<data_type, vector_allocator>> {
+        static_assert(std::is_integral<data_type>::value && std::is_unsigned<data_type>::value,
+                      "Only unsigned integers are supported");
 #ifndef GNUC
-		&& !std::is_same<data_type, std::uint64_t>::value //only supported when using gcc or clang
-#endif // GNUC
-			>::type>
-		class bigint {
-	protected:
-		using data_container_type = std::vector<data_type, vector_allocator>;
-		using base_type = typename boost::uint_t<sizeof(data_type) * 16 /* = 2*8 */>::exact;
-		data_container_type data; /*! < container containing the data */
-		
-		/*!
-			\brief Complements the data.
-			\detail The complement is a complement on base.
-			\sa base, undo_complement()
-		*/
-		void make_complement() noexcept;
+        static_assert(!std::is_same<data_type, std::uint64_t>::value, "Not supported when not using gcc or clang");
+#endif
+      protected:
+        using data_container_type = std::vector<data_type, vector_allocator>;                //!< the type of data
+        using base_type = typename boost::uint_t<sizeof(data_type) * 16 /* = 2*8 */>::exact; //!< the type of base
+        data_container_type data; //!< container containing the data
 
-		/*!
-			\brief Undos the complementation of the data which was complemented by make_complement().
-			\sa make_complement()
-		*/
-		void undo_complement() noexcept;
+        /*!
+                \brief Complements the data.
+                \details The complement is a complement on base.
+                \sa base, undo_complement()
+        */
+        void make_complement() noexcept;
 
-		/*!
-			\brief Removes all redundant zeros.
-		*/
-		void remove_zeros() noexcept;
+        /*!
+                \brief Undos the complementation of the data which was complemented by make_complement().
+                \sa make_complement()
+        */
+        void undo_complement() noexcept;
 
-		bool is_signed = false; /*! < Is true if the represented number is signed, else false. */
-		constexpr static base_type base = static_cast<base_type>(std::numeric_limits<data_type>::max()) + 1; /*! the base of the number system */
-		constexpr static data_type base_min_one = std::numeric_limits<data_type>::max(); /*! the base-1 of the number system */
+        /*!
+                \brief Removes all redundant zeros.
+        */
+        void remove_zeros() noexcept;
 
-	public:
-		using size_type = decltype(data.size()); /*! the size type used by this class */
-		bigint(data_type n);
-		bigint(std::int32_t n);
-		bigint(std::int64_t n);
-		bigint(std::string s);
-		bigint(const bigint& other) : data(other.data), is_signed(other.is_signed) {}
-		void operator++() noexcept;
-		bigint operator+(const bigint& other) noexcept;
-		bigint operator-(const bigint& other) noexcept;
-		bigint operator*(const bigint& other) noexcept;
-		bigint operator/(const bigint& other) noexcept;
-		bigint& operator+=(bigint other) noexcept;
-		bigint& operator-=(bigint other) noexcept;
-		bigint& operator*=(bigint other) noexcept;
-		bigint& operator/=(bigint other) noexcept;
-		bool operator<(const bigint& other) const noexcept;
-		bool operator>(const bigint& other) const noexcept;
-		bool operator==(const bigint& other) const noexcept;
-		bool operator!=(const bigint& other) const noexcept;
-		bool operator<=(const bigint& other) const noexcept;
-		bool operator>=(const bigint& other) const noexcept;
-		std::string to_string() const noexcept;
-		inline size_type size();
-		virtual ~bigint() = default;
-	};
+        bool is_signed = false; //!< is true if the represented number is signed, else false
+        constexpr static base_type base =
+            static_cast<base_type>(std::numeric_limits<data_type>::max()) + 1; //!< the base of the number system
+        constexpr static data_type base_min_one =
+            std::numeric_limits<data_type>::max(); //!< the base-1 of the number system
 
-	std::ostream& operator<<(std::ostream& out, const bigint& b)
-	{
-		return out << b.to_string();
-	}
+      public:
+        using size_type = decltype(data.size()); //!< the size type used by this class
+        bigint(data_type n) noexcept;
+        bigint(std::int64_t n) noexcept;
+        bigint(std::string hex_str);
+        bigint(const bigint& other) : data(other.data), is_signed(other.is_signed) {}
+        bigint& operator++() noexcept;
+        bigint& operator--() noexcept;
+        bigint& operator+=(bigint other) noexcept;
+        bigint& operator-=(bigint other) noexcept;
+        bigint& operator*=(bigint other) noexcept;
+        bigint& operator/=(bigint other) noexcept;
+        bool operator<(const bigint& other) const noexcept;
+        bool operator>(const bigint& other) const noexcept;
+        bool operator==(const bigint& other) const noexcept;
+        bool operator!=(const bigint& other) const noexcept;
+        bool operator<=(const bigint& other) const noexcept;
+        bool operator>=(const bigint& other) const noexcept;
+        std::string to_string() const noexcept;
+        inline size_type size();
+        virtual ~bigint() = default;
+    };
 
-	template class bigint<uint8_t>;
-	template class bigint<uint16_t>;
-	template class bigint<uint32_t>;
+    template <typename data_type, class vector_allocator = std::allocator<data_type>>
+    std::ostream& operator<<(std::ostream& out, const bigint<data_type, vector_allocator>& b) {
+        return out << b.to_string();
+    }
+
+    template class bigint<std::uint8_t>;
+    template class bigint<std::uint16_t>;
+    template class bigint<std::uint32_t>;
 #ifdef GNUC
-	template class bigint<uint64_t>; /*! < only supported when using gcc or clang*/
-#endif // GNUC
+    template class bigint<std::uint64_t>; // only supported when using gcc or clang
+#endif                                  
 
 } // namespace numeric
